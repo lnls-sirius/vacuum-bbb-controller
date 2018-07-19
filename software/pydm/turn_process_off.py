4711705631157b57499c9ecb798854f60419d07b
Rafael Ito <ito.rafael@gmail.com>
2018-07-19 09:47:41 -0300

    added logo files for UI and PV name list

--- software/pydm/turn_process_off.py
+++ software/pydm/turn_process_off.py
@@ -19,34 +19,40 @@ GATE_VALVE = "VBC:BBB:Relay4"
 # Stage 1:
 #==============================================================================
 # close pre-vacuum valve (and keeps gate valve open)
+caput("VBC:ProcessOff:Status1", 1)
 caput(PRE-VACUUM_VALVE, 0)
 #==============================================================================
 # Stage 2:
 #==============================================================================
 # turn TURBOVAC and ACP15 pumps OFF
+caput("VBC:ProcessOff:Status2", 1)
 caput("VBC:TURBOVAC:OnOff", 0)
 caput("VBC:ACP:OnOff", 0)
 #==============================================================================
 # Stage 3:
 #==============================================================================
 # wait until TURBOVAC frequency decrease to 600 Hz
+caput("VBC:ProcessOff:Status3", 1)
 while (caget("VBC:TURBOVAC:PZD2-RB") > 600):
     pass
 #==============================================================================
 # Stage 4:
 #==============================================================================
 # open X203 valve (TURBOVAC venting valve)
+caput("VBC:ProcessOff:Status4", 1)
 caput("VBC:TURBOVAC:PZD1-SP.FFVL", 1)
 #==============================================================================
 # Stage 5:
 #==============================================================================
 # wait until pressure gets 760 Torr
+caput("VBC:ProcessOff:Status5", 1)
 while (caget("VBC:BBB:Torr") < 760):
     pass
 #==============================================================================
 # Stage 6:
 #==============================================================================
 # close all the valves (gate valve is already closed)
+caput("VBC:ProcessOff:Status6", 1)
 caput("VBC:TURBOVAC:PZD1-SP.FFVL", 0)       # close X203
 caput(PRE-VACUUM_VALVE, 0)
 #==============================================================================
--- software/pydm/turn_process_on.py
+++ software/pydm/turn_process_on.py
@@ -18,12 +18,14 @@ GATE_VALVE = "VBC:BBB:Relay4"
 # Stage 1:
 #==============================================================================
 # open gate valve (VAT) and the pre-vacuum valve
+caput("VBC:ProcessOn:Status1", 1)
 caput(GATE_VALVE, 1)
 caput(PRE-VACUUM_VALVE, 1)
 #==============================================================================
 # Stage 2:
 #==============================================================================
 # read gate valve (VAT) status to check if it is really closed
+caput("VBC:ProcessOn:Status2", 1)
 loop = True
 while (loop):
     Lo = caget("VBC:BBB:ValveOpen")
@@ -34,17 +36,20 @@ while (loop):
 # Stage 3:
 #==============================================================================
 # turn ACP15 pump ON
+caput("VBC:ProcessOn:Status3", 1)
 caput("VBC:ACP15:OnOff", 1)
 #==============================================================================
 # Stage 4:
 #==============================================================================
 # read the pressure and proceed when its value is under 5*(10^-2) Torr
+caput("VBC:ProcessOn:Status4", 1)
 while (caget("VBC:BBB:Torr") > 0.05):
     pass
 #==============================================================================
 # Stage 5:
 #==============================================================================
 # turn TURBOVAC pump ON
+caput("VBC:ProcessOn:Status5", 1)
 caput("VBC:TURBOVAC:PZD1-SP.ZRVL", 1)
 caput("VBC:TURBOVAC:PZD1-SP.TEVL", 1)
 #==============================================================================
