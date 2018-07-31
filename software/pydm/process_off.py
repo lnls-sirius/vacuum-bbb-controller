699a1be4c52abc22870ca83e31a783cf072b5cb7
Rafael Ito <ito.rafael@gmail.com>
2018-07-31 10:46:49 -0300

    added support for parameterized process scripts

--- software/pydm/process_off.py
+++ software/pydm/process_off.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from epics import caget, caput
+import sys
 #------------------------------------------------------------------------------
 '''
 this script do all the procedures turn off the system with full ventilation
@@ -12,60 +13,58 @@ it is divided in 6 stages, described as follow:
     -stage 6: close X203 and gate valves
 '''
 #------------------------------------------------------------------------------
+# define the PREFIX that will be used (passed as a parameter)
+VBC = sys.argv[1]
+#------------------------------------------------------------------------------
 # valve names definition
-PRE_VACUUM_VALVE = "VBC:BBB:Relay2"
-GATE_VALVE = "VBC:BBB:Relay4"
+PRE_VACUUM_VALVE = VBC + ":BBB:Relay2"
+GATE_VALVE = VBC + ":BBB:Relay4"
+#------------------------------------------------------------------------------
+# clear all status PVs
+caput(VBC + ":ProcessOffFV:Status1", 1)
+caput(VBC + ":ProcessOffFV:Status1", 2)
+caput(VBC + ":ProcessOffFV:Status1", 3)
+caput(VBC + ":ProcessOffFV:Status1", 4)
+caput(VBC + ":ProcessOffFV:Status1", 5)
+caput(VBC + ":ProcessOffFV:Status1", 6)
 #==============================================================================
 # Stage 1:
 #==============================================================================
 # close pre-vacuum valve (and keeps gate valve open)
-import time
-print "Stage 1"
-time.sleep(5)
-caput("VBC:ProcessOffNV:Status1", 1)
+caput(VBC + ":ProcessOffFV:Status1", 1)
 caput(PRE_VACUUM_VALVE, 0)
 #==============================================================================
 # Stage 2:
 #==============================================================================
 # turn TURBOVAC and ACP15 pumps OFF
-print "Stage 2"
-time.sleep(5)
-caput("VBC:ProcessOffNV:Status2", 1)s
-caput("VBC:TURBOVAC:OnOff", 0)
-caput("VBC:ACP:OnOff", 0)
+caput(VBC + ":ProcessOffFV:Status2", 1)
+caput(VBC + ":TURBOVAC:PZD1-SP.ZRVL", 0)
+caput(VBC + ":ACP:OnOff", 0)
 #==============================================================================
 # Stage 3:
 #==============================================================================
 # wait until TURBOVAC frequency decrease to 600 Hz
-print "Stage 3"
-time.sleep(5)
-caput("VBC:ProcessOffNV:Status3", 1)
-while (caget("VBC:TURBOVAC:PZD2-RB") > 600):
+caput(VBC + ":ProcessOffFV:Status3", 1)
+while (caget(VBC + ":TURBOVAC:PZD2-RB") > caget(VBC + ":SYSTEM:OffFrequency"):
     pass
 #==============================================================================
 # Stage 4:
 #==============================================================================
 # open X203 valve (TURBOVAC venting valve)
-print "Stage 4"
-time.sleep(5)
-caput("VBC:ProcessOffNV:Status4", 1)
-caput("VBC:TURBOVAC:PZD1-SP.FFVL", 1)
+caput(VBC + ":ProcessOffFV:Status4", 1)
+caput(VBC + ":TURBOVAC:PZD1-SP.FFVL", 1)
 #==============================================================================
 # Stage 5:
 #==============================================================================
 # wait until pressure gets 760 Torr
-print "Stage 6"
-time.sleep(5)
-caput("VBC:ProcessOffNV:Status5", 1)
-while (caget("VBC:BBB:Torr") < 760):
+caput(VBC + ":ProcessOffFV:Status5", 1)
+while (caget(VBC + ":BBB:Torr") < (caget(VBC + ":SYSTEM:OffPressureBase") * 10 ** caget(VBC + ":SYSTEM:OffPressureExp")):
     pass
 #==============================================================================
 # Stage 6:
 #==============================================================================
 # close all the valves (gate valve is already closed)
-print "Stage 6"
-time.sleep(5)
-caput("VBC:ProcessOffNV:Status6", 1)
-caput("VBC:TURBOVAC:PZD1-SP.FFVL", 0)       # close X203
+caput(VBC + ":ProcessOffFV:Status6", 1)
+caput(VBC + ":TURBOVAC:PZD1-SP.FFVL", 0)       # close X203
 caput(PRE_VACUUM_VALVE, 0)
 #==============================================================================
--- software/pydm/process_on.py
+++ software/pydm/process_on.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from epics import caget, caput
+import sys
 #------------------------------------------------------------------------------
 '''
 this script do all the procedures to decrease the pressure of the system
@@ -11,52 +12,55 @@ it is divided in 5 stages, described as follow:
     -stage 5: turn TURBOVAC on
 '''
 #------------------------------------------------------------------------------
+# define the PREFIX that will be used (passed as a parameter)
+VBC = sys.argv[1]
+#------------------------------------------------------------------------------
 # valve names definition
-PRE_VACUUM_VALVE = "VBC:BBB:Relay2"
-GATE_VALVE = "VBC:BBB:Relay4"
+PRE_VACUUM_VALVE = VBC + ":BBB:Relay2"
+GATE_VALVE = VBC + ":BBB:Relay4"
 #------------------------------------------------------------------------------
 # clear all status PVs
-caput("VBC:ProcessOn:Status1", 1)
-caput("VBC:ProcessOn:Status1", 2)
-caput("VBC:ProcessOn:Status1", 3)
-caput("VBC:ProcessOn:Status1", 4)
-caput("VBC:ProcessOn:Status1", 5)
+caput(VBC + ":ProcessOn:Status1", 1)
+caput(VBC + ":ProcessOn:Status1", 2)
+caput(VBC + ":ProcessOn:Status1", 3)
+caput(VBC + ":ProcessOn:Status1", 4)
+caput(VBC + ":ProcessOn:Status1", 5)
 #==============================================================================
 # Stage 1:
 #==============================================================================
 # open gate valve (VAT) and the pre-vacuum valve
-caput("VBC:ProcessOn:Status1", 1)
+caput(VBC + ":ProcessOn:Status1", 1)
 caput(GATE_VALVE, 1)
 caput(PRE_VACUUM_VALVE, 1)
 #==============================================================================
 # Stage 2:
 #==============================================================================
 # read gate valve (VAT) status to check if it is really closed
-caput("VBC:ProcessOn:Status2", 1)
+caput(VBC + ":ProcessOn:Status2", 1)
 loop = True
 while (loop):
-    Lo = caget("VBC:BBB:ValveOpen")
-    Lg = caget("VBC:BBB:ValveClosed")
+    Lo = caget(VBC + ":BBB:ValveOpen")
+    Lg = caget(VBC + ":BBB:ValveClosed")
     if (Lo & (not Lg)):
         loop = False
 #==============================================================================
 # Stage 3:
 #==============================================================================
 # turn ACP15 pump ON
-caput("VBC:ProcessOn:Status3", 1)
-caput("VBC:ACP:OnOff", 1)
+caput(VBC + ":ProcessOn:Status3", 1)
+caput(VBC + ":ACP:OnOff", 1)
 #==============================================================================
 # Stage 4:
 #==============================================================================
 # read the pressure and proceed when its value is under 5*(10^-2) Torr
-caput("VBC:ProcessOn:Status4", 1)
-while (caget("VBC:BBB:Torr") > 0.08):
+caput(VBC + ":ProcessOn:Status4", 1)
+while (caget(VBC + ":BBB:Torr") > (caget(VBC + ":SYSTEM:OnPressureBase") * 10 ** caget(VBC + ":SYSTEM:OnPressureExp")):
     pass
 #==============================================================================
 # Stage 5:
 #==============================================================================
 # turn TURBOVAC pump ON
-caput("VBC:ProcessOn:Status5", 1)
-caput("VBC:TURBOVAC:PZD1-SP.ZRVL", 1)
-caput("VBC:TURBOVAC:PZD1-SP.TEVL", 1)
+caput(VBC + ":ProcessOn:Status5", 1)
+caput(VBC + ":TURBOVAC:PZD1-SP.ZRVL", 1)
+caput(VBC + ":TURBOVAC:PZD1-SP.TEVL", 1)
 #==============================================================================
